386c76e1138fb2d672fb7e22a1088eec784b9ed4
Roman.Kydybets <kydybets@gmail.com>
2017-04-25 22:07:13 +0300

    new app

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["Hello World Docker test"]
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World from Flask"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=80)
