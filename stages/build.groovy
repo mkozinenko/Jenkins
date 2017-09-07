ac3271eadaf82b8eebaff5a16860e8ef5cc49673
kydybets <kydybets@gmail.com>
2017-09-07 17:30:24 +0300

    edit seed jon file

--- stages/build.groovy
+++ stages/build.groovy
@@ -1,3 +1,3 @@
-// cp /var/lib/jenkins/workspace/Demo2/build/hello.py /var/lib/jenkins/workspace/Demo2/deploy/
+# cp /var/lib/jenkins/workspace/Demo2/build/hello.py /var/lib/jenkins/workspace/Demo2/deploy/
 
 echo "build.groovy"
--- stages/test.groovy
+++ stages/test.groovy
@@ -1,3 +1,3 @@
-// pylint '/var/lib/jenkins/workspace/Demo2/test/hello.py' || cp '/var/lib/jenkins/workspace/Demo2/test/hello.py' /var/lib/jenkins/workspace/Demo2/build/
+# pylint '/var/lib/jenkins/workspace/Demo2/test/hello.py' || cp '/var/lib/jenkins/workspace/Demo2/test/hello.py' /var/lib/jenkins/workspace/Demo2/build/
 
 echo "test groovy"
